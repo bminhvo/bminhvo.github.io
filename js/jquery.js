b8027b109763415fe394a323bec7177520bfdae3
Minh Vo <bminhvo@gmail.com>
2017-12-22 15:27:57 -0500

    Add script, fix error

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#projects").on("click", function(){
-        $("#content").load("views/projects.html");
+        $("#content").load("../views/projects.html");
     });
     // $("#contact").click(function(){
         // $("#content").load("./contact.html");
